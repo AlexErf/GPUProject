4083e09bf7df1c36a01cd999e21c4312c49a3c7f
Eric Mei <ericmei@umich.edu>
2020-11-13 15:56:55 -0500

    Apply mypass to the memeset-external. Fix opencl lib path.

--- opencl-examples/memset/compile.sh
+++ opencl-examples/memset/compile.sh
@@ -2,4 +2,4 @@
 
 clang++ -emit-llvm main.cpp -c -o main.bc
 opt -o main.pass.bc -load ~/583Project/build/mypass/LLVMPJT.so -hello < main.bc > /dev/null
-clang main.pass.bc -o main -lOpenCL -L/opt/amdgpu-pro/lib/x86_64-linux-gnu/
+clang main.pass.bc -o main -lOpenCL -L/opt/rocm/opencl/lib/
