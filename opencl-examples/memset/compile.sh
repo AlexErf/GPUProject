7e1c780d089514322e293c4051cf101293a4c777
Nathan Brown <nlbrow@umich.edu>
2020-11-23 18:16:17 -0500

    Merge remote branch

--- opencl-examples/memset/compile.sh
+++ opencl-examples/memset/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 clang++ -emit-llvm main.cpp -c -o main.bc
-opt -o main.pass.bc -load ~/583Project/build/mypass/LLVMPJT.so -hello < main.bc > /dev/null
+opt -o main.pass.bc -load ~/GPUProject/build/mypass/LLVMPJT.so -hello < main.bc > /dev/null
 clang main.pass.bc -o main -lOpenCL -L/opt/rocm/opencl/lib/
