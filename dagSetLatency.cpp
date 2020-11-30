d8fd5be0083018fd62a1b07fb2a08613eb32e8c8
Eric Mei <ericmei@umich.edu>
2020-11-30 17:59:59 -0500

    Add function to restore latencies

--- dagSetLatency.cpp
+++ dagSetLatency.cpp
@@ -38,10 +38,12 @@ static std::map<SUnit*, node> setLatenciesToOne(std::vector<SUnit*> topRoots) {
     // a mapping from original graph to the clone graph
     std::map<SUnit*, node> mapSUnitToNode;
 
-    std::queue<SUnit*> queue;
+    std::deque<SUnit*> queue;
+    std::vector<SUnit*> visited;
 
     for (auto it =  topRoots.begin(); it != topRoots.end(); ++it) {
         queue.push(*it);
+        visited.push_back(*it);
     }
 
     // initialize a one-to-one mapping from the original SUnit to node
@@ -54,12 +56,17 @@ static std::map<SUnit*, node> setLatenciesToOne(std::vector<SUnit*> topRoots) {
         SmallVector<SDep, 4> succs = sunit -> Succs;
         for (auto it = succs.begin(); it != succs.end(); ++it) {
             SUnit* succ = it->getSUnit();
-            queue.push(succ);
+            if (find(visited.begin(), visited.end(), succ) == visited.begin()){
+                queue.push(succ);
+                visited.push_back(succ)
+            }
         }
     }
 
+    visited.clear();
     for (auto it =  topRoots.begin(); it != topRoots.end(); ++it) {
         queue.push(*it);
+        visited.push_back(*it);
     }
 
     // build the clone graph and set all original latencies to one
@@ -67,12 +74,12 @@ static std::map<SUnit*, node> setLatenciesToOne(std::vector<SUnit*> topRoots) {
         SUnit* sunit = queue.front();
         queue.pop();
 
-        node currentNode = mapSUnitToNode[sunit];
+        node& currentNode = mapSUnitToNode[sunit];
         SmallVector<SDep, 4> succs = sunit -> Succs;
         for (auto it = succs.begin(); it != succs.end(); ++it) {
             SUnit* succ = it->getSUnit();
             int latency = it->getLatency();
-            node succNode = mapSUnitToNode[succ];
+            node& succNode = mapSUnitToNode[succ];
 
             // set current node as successor's pred
             succNode.preds.push_back(currentNode);
@@ -85,8 +92,41 @@ static std::map<SUnit*, node> setLatenciesToOne(std::vector<SUnit*> topRoots) {
             // set latency to 1
             it->setLatency(1);
 
-            queue.push(succ);
+            // push succ if it is not in the queue or visited
+            if (find(visited.begin(), visited.end(), succ) == visited.end()){
+                queue.push(succ);
+                visited.push_back(succ)
+            }
         }
     }
     return mapSUnitToNode;
+}
+
+static void restoreLatencies(std::vector<SUnit*> topRoots, std::map<SUnit*, node> mapSUnitToNode) {
+    std::queue<SUnit*> queue;
+    std::vector<SUnit*> visited;
+
+    for (auto it : topRoots) {
+        queue.push(*it);
+        visited.push(*it);
+    }
+
+    while (!queue.empty()) {
+        SUnit* sunit = queue.front();
+        queue.pop();
+
+        SmallVector<SDep, 4> succs = it -> Succs;
+        node currentNode = mapSUnitToNode[it];
+        for (auto dep : succs) {
+           SUnit* succ = dep.getSUnit();
+           node succNode = mapSUnitToNode[succ];
+           int latency = currentNode.getSuccLatency(succNode);
+           dep.setLatency(latency);
+
+           if (find(visited.begin(), visited.end(), succ) == visited.end()){
+                queue.push(succ);
+                visited.push_back(succ)
+            }
+        }
+    }
 }
