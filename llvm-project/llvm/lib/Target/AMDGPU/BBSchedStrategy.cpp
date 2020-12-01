c7ebaa1eda74fcf860e0d0de3c3d7d8051a58568
Eric Mei <ericmei@umich.edu>
2020-11-30 22:32:51 -0500

    skeleton code for schedule

--- llvm-project/llvm/lib/Target/AMDGPU/BBSchedStrategy.cpp
+++ llvm-project/llvm/lib/Target/AMDGPU/BBSchedStrategy.cpp
@@ -325,6 +325,22 @@ GCNScheduleDAGMILive::GCNScheduleDAGMILive(MachineSchedContext *C,
  * }
  */
 
+void GCNScheduleDAGMILive::occupancyPass(std::vector<SUnit* > topRoots, int targetPressure) {
+  std::map<SUnit*, node> mapSUnitToNode = setLatenciesToOne(topRoots);
+  // enumerate();
+  restoreLatencies(topRoots, mapSUnitToNode);
+}
+
+void GCNScheduleDAGMILive::ilpPass(std::vector<SUnit* > topRoots, std::vector<SUnit* > scheduleInst, int targetPressure) {
+  int lowerBound = std::distance(Regions[RegionIdx].first, Regions[RegionIdx].second);
+  int upperBound = satisfyLatency(scheduleInst);
+  for (int targetLength = lowerBound; targetLength < upperBound - 1; targetLength ++) {
+    /* run B&B, if such schedule length exist, return */
+    // if (enumerate())
+    //   return;
+  }
+}
+
 void GCNScheduleDAGMILive::schedule() {
     /*
      * TODO: actually implement this
@@ -381,6 +397,12 @@ void GCNScheduleDAGMILive::schedule() {
   ScheduleDAGMILive::schedule();
   Regions[RegionIdx] = std::make_pair(RegionBegin, RegionEnd);
   RescheduleRegions[RegionIdx] = false;
+
+  buildDAGWithRegPressure();
+  Topo.InitDAGTopologicalSorting();
+  SmallVector<SUnit*, 8> TopRoots;
+  SmallVector<SUnit*, 8> BotRoots;
+  findRootsAndBiasEdges(TopRoots, BotRoots);
 
   if (!LIS)
     return;
@@ -676,4 +698,166 @@ void GCNScheduleDAGMILive::finalizeSchedule() {
     if (Stage == UnclusteredReschedule)
       SavedMutations.swap(Mutations);
   } while (Stage != LastStage);
+}
+
+struct node {
+    std::vector<node> preds;
+    std::vector<int> predsLatencies;
+
+    std::vector<node> succs;
+    std::vector<int> succsLatencies;
+
+    /// a helper function to get the succ latency
+    int getSuccLatency(node succ) {
+        auto it = find(succs.begin(), succs.end(), succs);
+
+        if (it != succs.end()) {
+            int index = it - succs.begin();
+            return succsLatencies[index];
+        }
+    }
+
+    /// a helper function to get the pred latency
+    int getPredLatency(node pred) {
+        auto it = find(preds.begin(), preds.end(), pred);
+
+        if (it != preds.end()) {
+            int index = it - preds.begin();
+            return predsLatencies[index];
+        }
+    }
+};
+
+static std::map<SUnit*, node> setLatenciesToOne(std::vector<SUnit*> &topRoots) {
+    // a mapping from original graph to the clone graph
+    std::map<SUnit*, node> mapSUnitToNode;
+
+    std::deque<SUnit*> queue;
+    std::vector<SUnit*> visited;
+
+    for (auto it =  topRoots.begin(); it != topRoots.end(); ++it) {
+        queue.push(*it);
+        visited.push_back(*it);
+    }
+
+    // initialize a one-to-one mapping from the original SUnit to node
+    while (!queue.empty()) {
+        SUnit* sunit = queue.front();
+        queue.pop();
+
+        struct node clone;
+        mapSUnitToNode.insert({sunit, clone});
+        SmallVector<SDep, 4> succs = sunit -> Succs;
+        for (auto it = succs.begin(); it != succs.end(); ++it) {
+            SUnit* succ = it->getSUnit();
+            if (find(visited.begin(), visited.end(), succ) == visited.begin()){
+                queue.push(succ);
+                visited.push_back(succ)
+            }
+        }
+    }
+
+    visited.clear();
+    for (auto it =  topRoots.begin(); it != topRoots.end(); ++it) {
+        queue.push(*it);
+        visited.push_back(*it);
+    }
+
+    // build the clone graph and set all original latencies to one
+    while (!queue.empty()) {
+        SUnit* sunit = queue.front();
+        queue.pop();
+
+        node& currentNode = mapSUnitToNode[sunit];
+        SmallVector<SDep, 4> succs = sunit -> Succs;
+        for (auto it = succs.begin(); it != succs.end(); ++it) {
+            SUnit* succ = it->getSUnit();
+            int latency = it->getLatency();
+            node& succNode = mapSUnitToNode[succ];
+
+            // set current node as successor's pred
+            succNode.preds.push_back(currentNode);
+            succNode.predsLatencies.push_back(latency);
+
+            // set successor as current node's succ
+            currentNode.succs.push_back(succNode);
+            currentNode.succsLatencies.push_back(latency);
+
+            // set latency to 1
+            it->setLatency(1);
+
+            // push succ if it is not in the queue or visited
+            if (find(visited.begin(), visited.end(), succ) == visited.end()){
+                queue.push(succ);
+                visited.push_back(succ)
+            }
+        }
+    }
+    return mapSUnitToNode;
+}
+
+static void restoreLatencies(std::vector<SUnit*> &topRoots, std::map<SUnit*, node> mapSUnitToNode) {
+    std::queue<SUnit*> queue;
+    std::vector<SUnit*> visited;
+
+    for (auto it : topRoots) {
+        queue.push(*it);
+        visited.push(*it);
+    }
+
+    while (!queue.empty()) {
+        SUnit* sunit = queue.front();
+        queue.pop();
+
+        SmallVector<SDep, 4> succs = it -> Succs;
+        node currentNode = mapSUnitToNode[it];
+        for (auto dep : succs) {
+           SUnit* succ = dep.getSUnit();
+           node succNode = mapSUnitToNode[succ];
+           int latency = currentNode.getSuccLatency(succNode);
+           dep.setLatency(latency);
+
+           if (find(visited.begin(), visited.end(), succ) == visited.end()){
+                queue.push(succ);
+                visited.push_back(succ)
+            }
+        }
+    }
+}
+
+/// Compute how many stall need to be inserted to satisfy the latency
+static int satisfyLatency(std::vector<SUnit*> schedule) {
+    int current_cycle = 0;
+    int index = 0;
+
+    std::map<SDep, int> sched_sdep;
+
+    while (index < schedule.size()) {
+        SUnit* sunit = schedule[index];
+        SmallVector<SDep, 4> preds = sunit -> Preds;
+
+        bool can_schedule = true;
+        for (auto pred : preds) {
+            if (sched_sdep[pred] > 0) {
+                can_schedule = false;
+            }
+        }
+        if (can_schedule) {
+            for (auto succ : sunit -> Succs) {
+                int latency = succ.getLatency();
+                sched_sdep.insert(std::make_pair(succ, latency));
+            }
+            // schedule next inst
+            index ++;
+        }
+
+        // increase cycle
+        current_cycle ++;
+
+        // decrease all scheduled latency
+        for (std::pair<SDep, int> element : sched_sdep) {
+            sched_sdep[element.first] --;
+        }
+    }
+    return current_cycle;
 }
