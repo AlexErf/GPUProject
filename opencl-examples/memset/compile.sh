7f779213a8ba93ef7cef689722f7977d4b75dddb
Alex Erf <alexerf@umich.edu>
2020-12-15 23:04:47 -0500

    removed unneeded files

--- opencl-examples/memset/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-clang++ -emit-llvm main.cpp -c -o main.bc
-opt -o main.pass.bc -load ~/GPUProject/build/mypass/LLVMPJT.so -hello < main.bc > /dev/null
-clang main.pass.bc -o main -lOpenCL -L/opt/rocm/opencl/lib/
